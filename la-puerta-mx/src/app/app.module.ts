ac1114aed77098ec59fd5577961402c6acfdbbca
William Morales <williamorggan@gmail.com>
2024-08-16 20:09:24 -0600

    WIP: Add some features in calculations to Florencia Classic

--- la-puerta-mx/src/app/app.module.ts
+++ la-puerta-mx/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { ClosetsComponent } from './features/calculators/closets/closets/closets.component';
+import { ClosetModalComponent } from './features/home/closet-modal/closet-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ClosetsComponent
+    ClosetsComponent,
+    ClosetModalComponent
   ],
   imports: [
     BrowserModule,
