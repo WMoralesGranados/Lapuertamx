3daeea0a26f1fb2e97fac341bed6fd453ad0cf39
William Morales <williamorggan@gmail.com>
2024-09-10 21:11:20 -0600

    Fix: position fo Alto input

--- la-puerta-mx/src/app/app-routing.module.ts
+++ la-puerta-mx/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { ClosetsComponent } from './features/calculators/closets/closets/closets.component';
+import { ClosetsComponent } from './features/calculators/closets/Florencia-Classic/closets.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
