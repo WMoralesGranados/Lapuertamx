2bc8a7bac21fd3bb4e48a2eb9ab92c59b807400b
William Morales <williamorggan@gmail.com>
2024-10-31 18:41:56 -0600

    Feat: add florencia classic modify component

---
+++ la-puerta-mx/src/app/features/calculators/closets/fc-cajonera-costado/fc-cajonera-costado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fc-cajonera-costado',
+  templateUrl: './fc-cajonera-costado.component.html',
+  styleUrl: './fc-cajonera-costado.component.css'
+})
+export class FCCajoneraCostadoComponent {
+
+}
