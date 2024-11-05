28c525e9eb8edec5968dac4cd0e17aba132f35ab
William Morales <williamorggan@gmail.com>
2024-11-04 20:05:54 -0600

    Deploy Angular app to GitHub Pages

---
+++ la-puerta-mx/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GQUH6C2E.mjs";import"./chunk-AQGROGK5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
